30c4122c37bbcaa2069f4ac85b9e637b8487fbd0
Balkrishna Rawool <balkrishna.rawool@gmail.com>
2024-09-13 00:36:53 +0200

    combine() is simplified using CF.thenCombine()

--- src/main/java/com/balarawool/CompletableFutures.java
+++ src/main/java/com/balarawool/CompletableFutures.java
@@ -22,21 +22,16 @@ public interface CompletableFutures {
     static <T> CompletableFuture<Stream<T>> sequence(Stream<CompletableFuture<T>> futures) {
         return futures
                 .map(future -> future.thenApply(Stream::of))
-                .reduce(
-                        CompletableFuture.completedFuture(Stream.empty()),
-                        CompletableFutures::combine);
-    }
-
-    // Copied from: https://stackoverflow.com/questions/48461315/how-to-transform-completablefuturestreamt-to-streamcompletablefuturet
-    static <T>CompletableFuture<Stream<T>> combine(CompletableFuture<Stream<T>> future1, CompletableFuture<Stream<T>> future2) {
-        return future1
-                .thenCompose(stream1 -> future2
-                        .thenApply(stream2 -> Stream.concat(stream1, stream2)));
+                .reduce(CompletableFuture.completedFuture(Stream.empty()), CompletableFutures::combine);
     }
 
     static <T> CompletableFuture<Optional<T>> sequence(Optional<CompletableFuture<T>> optional) {
          return optional.map(future -> future.thenApply(Optional::of))
                  .orElse(CompletableFuture.completedFuture(Optional.empty()));
+    }
+
+    static <T> CompletableFuture<Stream<T>> combine(CompletableFuture<Stream<T>> f1, CompletableFuture<Stream<T>> f2) {
+        return f1.thenCombine(f2, Stream::concat);
     }
 
 }
